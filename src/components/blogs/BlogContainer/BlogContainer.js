9809cc0dbfa8786ca3a6512bce17c2619fbe8731
rzsaglam <rzn.saglam@gmail.com>
2022-03-30 16:28:10 +0300

    Blog Component Added

--- src/components/blogs/BlogContainer/BlogContainer.js
+++ src/components/blogs/BlogContainer/BlogContainer.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import styles from "./styles.module.css";
 
 function BlogContainer({ blogData }) {
+  const post =
+    blogData.post.length > 240
+      ? blogData.post.slice(0, 240) + "..."
+      : blogData.post;
   return (
-    <div>
-      {blogData.title}
-      {blogData.post}
-      {blogData.author}
+    <div className={styles.container}>
+      <div className={styles.author}>{blogData.author}</div>
+      <div className={styles.title}>{blogData.title}</div>
+      <div className={styles.post}>{post}</div>
     </div>
   );
 }
