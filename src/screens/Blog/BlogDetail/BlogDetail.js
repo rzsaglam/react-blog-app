09cea713df3aa5dec6cdcd254ff2c4f7bbefa97a
rzsaglam <rzn.saglam@gmail.com>
2022-04-02 18:15:32 +0300

    BlogDetail Screen Added

--- src/screens/Blog/BlogDetail/BlogDetail.js
+++ src/screens/Blog/BlogDetail/BlogDetail.js
@@ -1,18 +1,52 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AuthContext from "../../../contexts/AuthContext";
 import { useParams } from "react-router-dom";
+import { getBlogById } from "../../../library/network/requests/blogs";
+import { Bounce } from "react-activity";
+
 import styles from "./styles.module.css";
+import "react-activity/dist/library.css";
 
 import SideBar from "../../../components/navigation/SideBar/SideBar";
 
 function BlogDetail() {
   const { id } = useParams();
+  const [blog, setBlog] = useState({});
+  const [loading, setLoading] = useState(true);
   const { handleLogout, handleNewBlog } = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetch() {
+      setBlog((await getBlogById(id)).data);
+      setLoading(false);
+    }
+    fetch();
+  }, [id]);
 
   return (
     <div className={styles.container}>
-      <SideBar handleLogOut={handleLogout} handleNewBlog={handleNewBlog} />
-      <div className={styles.main}></div>
+      <div>
+        <SideBar handleLogOut={handleLogout} handleNewBlog={handleNewBlog} />
+      </div>
+      <div className={styles.main}>
+        {loading ? (
+          <Bounce
+            style={{
+              position: "fixed",
+              top: "50%",
+              left: "50%",
+              color: "#484848",
+            }}
+            size={15}
+          />
+        ) : (
+          <>
+            <div className={styles.title}>{blog.title}</div>
+            <div className={styles.post}>{blog.post}</div>
+            <div className={styles.author}>{blog.author}</div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
